a11662f0f1f8b320d35310235363d5edea0ec5f0
Andrew <andrewceng@gmail.com>
2020-01-20 11:35:41 +0000

    update

--- runme.sh
+++ runme.sh
@@ -2,5 +2,6 @@
 
 git clone https://github.com/acklab-ae/jupyter.git /tmp/update &&
 echo "Execute whatever script you want after launch" &&
-jupyter notebook --ip 0.0.0.0 --allow-root --no-browser
+# sh /tmp/update/update.sh &&
+jupyter notebook --ip 0.0.0.0 --allow-root --no-browser &
 
